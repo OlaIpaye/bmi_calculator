059ef4cddf8f61709ed49cc23cace4f94abe7167
Ola Ipaye <ipayeola@outlook.com>
2025-08-06 22:31:52 +0100

    updated the user inputs and bmi calculation with functions in order to make the code more modular and scalable.

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -8,15 +8,11 @@
 # 5. Categorize BMI
 # 6. Display result
 
-# Declaring variables for weight and height
-weight = float(input("Enter your weight in kg: "))
-height_cm = float(input("Enter your height in cm: "))
 
-# Converting height from cm to meters
-height_m = height_cm / 100
 
-# Calculating BMI
-bmi = weight / (height_m ** 2)
+
+
+
 
 # Categorizing BMI using if-elif-else statements
 if bmi < 18.5:
@@ -30,9 +26,49 @@ else:
 
 # Displaying the result
 print(f"Your BMI is: {bmi:.2f}. You are classified as: {category}.")
-print("Thank you for using the BMI Calculator!")
+print("Thank you for using our BMI Calculator!")
 
 # This code calculates the Body Mass Index (BMI) based on user input for weight and height.
 # End of code - Works in the terminal
+
+
+# Code refactor - making code more modular
+def get_user_input():
+    """ 
+        Declaring variables for weight and height for getting user inputs.
+        Prompts the user to enter his/her weight and height.
+        Returns:
+        tuple: weight in kg (float), height in cm (float)
+    """
+    weight = float(input("Enter your weight in kg: "))
+    height_cm = float(input("Enter your height in cm: "))
+    return weight, height_cm
+
+def calculate_bmi(weight, height_cm):
+    """
+        Converting height from cm to meters.
+        Then:
+        Calculating the bmi using the WHO standards, 
+        ready to be used for the conditional statements to
+        determine the category a user fits in.
+    
+    """
+    height_m = height_cm / 100
+    # Calculating BMI
+    bmi = weight / (height_m ** 2)
+    return bmi
+
+
+def main():
+    weight, height_cm = get_user_input()
+
+
+
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
 
 
