461885e549f257fb8c62e0b7463b24fc35836958
Ola Ipaye <ipayeola@outlook.com>
2025-08-07 18:51:01 +0100

    Updated the bmi_app.py file with full code for deploying the bmi calculator on the browser then refactored my code by using various streamlit functions such as number_input, title, subheader, and write to display the output on the browser.

--- bmi_app.py
+++ bmi_app.py
@@ -3,3 +3,27 @@ import streamlit as st
 st.title("BMI Calculator")
 
 st.write("Welcome! Use the inputs below to calculate your Body Mass Index (BMI).")
+
+# Created number inputs for weight and height to get user inputs.
+weight = st.number_input("Enter your weight in kg:", min_value=1.0, max_value=400.0, step=0.5)
+height_cm = st.number_input("Enter your height in cm:", min_value=50.0, max_value=400.0, step=0.5)
+
+# Calculate BMI
+if weight and height_cm:
+    height_m = height_cm / 100 # converts height to meters
+    bmi = weight / (height_m ** 2)
+
+    if bmi < 18.5:
+        category = "Underweight"
+    elif bmi < 25:
+        category = "Normal Weight"
+    elif bmi < 30:
+        category = "Overweight"
+    else:
+        category = "Obese"
+
+    # Display result
+    st.subheader("Your BMI Results:")
+    st.write(f"**BMI:** {bmi:.2f}. You are {category}!") # round the bmi figure to 2 decimal places
+
+
